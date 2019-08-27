667e85d1f0ed533a95e2687de50e61fba16b09ee
KoganezawaRyouta <ryota.koganezawa@gmail.com>
2019-08-27 15:00:56 +0900

    Initial Commit

---
+++ ios-sample-xx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-sample-xx
+//
+//  Created by Koganezawa on 2019/08/27.
+//  Copyright © 2019 Koganezawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
